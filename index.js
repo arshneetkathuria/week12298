b8e6365d13c60a6b9852a809cefd78e4f9069679
arshneetkathuria <arntkr13@gmail.com>
2024-07-31 11:07:37 -0400

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
+// Middlewaree
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
